6c367596d40dfc8376e98639eace4e2c4a535e4b
Bennett S. Owen <hertz@26hz.com.cn>
2024-12-05 10:59:59 +0800

    摩斯乱码辅码原为虎码前两码，修改为虎码首末字根大码

--- tools/00-ONE_KEY.sh
+++ tools/00-ONE_KEY.sh
@@ -4,9 +4,9 @@ python 02-schemagen.py --auxiliary-code=user update-compact-dict --rime-dict=./d
 
 python 03-convert_to_yaml_dict.py
 
-python 04-gen_moss_tiger_dict.py ../dicts/moss.basic.dict.yaml --dict data/moran.basic.dict.yaml
+python 04-gen_moss_tiger_dict.py ../dicts/moss.basic.dict.yaml --dict data/moran/moran.basic.dict.yaml
 python 04-gen_moss_tiger_dict.py ../dicts/moss.phrase.dict.yaml --dict data/phrase.dict.yaml
-python 04-gen_moss_tiger_dict.py ../dicts/moss.words.dict.yaml --dict data/moran.words.dict.yaml
-python 04-gen_moss_tiger_dict.py ../dicts/moss.tencent.dict.yaml --dict data/moran.tencent.dict.yaml
-python 04-gen_moss_tiger_dict.py ../dicts/moss.computer.dict.yaml --dict data/moran.computer.dict.yaml
-python 04-gen_moss_tiger_dict.py ../dicts/moss.moe.dict.yaml --dict data/moran.moe.dict.yaml
+python 04-gen_moss_tiger_dict.py ../dicts/moss.words.dict.yaml --dict data/moran/moran.words.dict.yaml
+python 04-gen_moss_tiger_dict.py ../dicts/moss.tencent.dict.yaml --dict data/moran/moran.tencent.dict.yaml
+python 04-gen_moss_tiger_dict.py ../dicts/moss.computer.dict.yaml --dict data/moran/moran.computer.dict.yaml
+python 04-gen_moss_tiger_dict.py ../dicts/moss.moe.dict.yaml --dict data/moran/moran.moe.dict.yaml
