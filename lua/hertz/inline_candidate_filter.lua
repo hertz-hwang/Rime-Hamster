88c6a2928364a2ec62dd4ac6bd3d4ffc1ba369bb
Bennett S. Owen <hertz@26hz.com.cn>
2024-12-08 11:32:43 +0800

    更新拆分表；更新tiger_spelling_pseudo.dict.yaml；更新tiger.yaml以启用部件内嵌；重构inline_candidate_filter.lua以利用自定义拆分表进行改进的候选过滤，首选内嵌 → 部件内嵌。

--- lua/hertz/inline_candidate_filter.lua
+++ lua/hertz/inline_candidate_filter.lua
@@ -1,7 +1,22 @@
+-- 加载自定义拆分表
+local spelling_table = require("tiger.spelling_table")
+
 local function filter(input)
     for cand in input:iter() do
-        cand:get_genuine().preedit = cand.text
+        local text = cand:get_genuine().text
+        local spellings = {}
+        
+        -- 遍历词组中的每个字
+        for char in text:gmatch("[%z\1-\127\194-\244][\128-\191]*") do
+            -- 从拆分表中获取每个字的拆分，如果没有则使用字本身
+            local spelling = spelling_table[char] or char
+            table.insert(spellings, spelling)
+        end
+        
+        -- 用空格连接所有拆分
+        cand:get_genuine().preedit = table.concat(spellings, " ")
         yield(cand)
     end
 end
+
 return filter
